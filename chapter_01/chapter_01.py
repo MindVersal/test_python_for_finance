cb4a8480a1ad52a49afd65bce8bd36e95122236d
MindVersal <antajchik@gmail.com>
2017-09-24 15:29:20 +0300

    15:30 24.09.2017

--- chapter_01/chapter_01.py
+++ chapter_01/chapter_01.py
@@ -1,4 +1,8 @@
-import datetime
+import datetime as dt
+import matplotlib.pyplot as plt
+from matplotlib import style
+import pandas as pd
+import pandas_datareader.data as web
 
 
 def test_zero():
@@ -9,6 +13,16 @@ def test_last():
     print('THE END.')
 
 
+def test_tsla_yahoo():
+    style.use('ggplot')
+    start = dt.datetime(2000, 1, 1)
+    end = dt.datetime(2012, 12, 31)
+    df = web.DataReader('TSLA', 'google', start, end)
+    print(df.tail(6))
+
+
 if __name__ == '__main__':
     test_zero()
+    test_tsla_yahoo()
+
     test_last()
