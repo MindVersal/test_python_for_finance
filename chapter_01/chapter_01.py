04e22c91afc871e8364e938577b10b67528ab06e
MindVersal <antajchik@gmail.com>
2017-09-24 15:01:18 +0300

    Initial commit

---
+++ chapter_01/chapter_01.py
@@ -0,0 +1,14 @@
+import datetime
+
+
+def test_zero():
+    print('Programming on python for finance.')
+
+
+def test_last():
+    print('THE END.')
+
+
+if __name__ == '__main__':
+    test_zero()
+    test_last()
