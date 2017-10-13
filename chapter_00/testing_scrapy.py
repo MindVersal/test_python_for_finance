e6bc97230350e8f78d8452e55952625c959168b8
MindVersal <antajchik@gmail.com>
2017-10-13 20:59:01 +0300

    21:51 13.10.2017

---
+++ chapter_00/testing_scrapy.py
@@ -0,0 +1,13 @@
+import bs4 as bs
+
+
+def test_avito():
+    print('Test Avito buisness')
+    url = [
+        'http://www.avito.ru/cherepovets/gotoviy_biznes'
+    ]
+
+
+if __name__ == '__main__':
+    print('Testing Scrapy Spider')
+    print()
